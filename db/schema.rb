01fa65d0c76610f8ded727b9f7ac3a70986e4d50
Joao <jooalberto1@gmail.com>
2024-10-31 23:50:29 -0300

    Change db from SQLite to PG

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_10_28_193347) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
